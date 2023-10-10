b65c61c53bdcd13be7645e141a72b9a37c2a5091
KhaiberNijat <khaiber.nijat77@gmail.com>
2023-10-10 06:10:23 +0200

    comment area

--- greetings.go
+++ greetings.go
@@ -2,11 +2,11 @@ package greetings
 
 import "fmt"
 
-// Hello returns a greeting for the name person.
-func Hello(Khaiber string) string{
 
-//return a greeting that embeds the name in a message.
-message :=fmt.Sprintf("Hi, %v. Welcome!", Khaiber)
-return message
+func Hello(Khaiber string) string
+{
+
+	message :=fmt.Sprintf("Hi, %v. Welcome!", Khaiber)
+	return message
 
 }
